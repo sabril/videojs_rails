9b74f0ca474226e007eaa2eef2abce859a2cfca0
Muhammad Syaiful Sabril <syaiful.sabril@gmail.com>
2013-10-07 20:36:43 +0700

    fixes

--- lib/videojs_rails/view_helpers.rb
+++ lib/videojs_rails/view_helpers.rb
@@ -2,6 +2,8 @@ module VideojsRails
   module ViewHelpers
     def videojs_rails(*options)
       @options = options.extract_options!
+      @options[:autoplay] ||= false
+      @options[:loop] ||= false
       render "videojs_rails/videojs_rails"
     end
   end
